15cb5eb430c032a0a4f389522815fc50f25e2029
Pete Hodgson <git@thepete.net>
2017-03-18 11:39:52 -0700

    basic styling

--- src/components/Station.js
+++ src/components/Station.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default function Station({station}){
-  return <li className="station">
+  return (
+    <li className="station-list__station">
       {station.name}
-    </li>;
+    </li>
+  );
 }
--- src/components/Stations.js
+++ src/components/Stations.js
@@ -5,7 +5,7 @@ import Station from './Station';
 export default function Stations({stations}){
   const listItems = stations.map( (station) => <Station station={station}/> );
   return (
-    <ul className="stationsList">
+    <ul className="station-list">
       {listItems}
     </ul>
   );
