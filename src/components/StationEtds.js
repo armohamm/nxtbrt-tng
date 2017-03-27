d90cbc088c6bc81e0498522004bef77162da9bbc
Pete Hodgson <git@thepete.net>
2017-03-26 22:56:42 -0700

    functional departures list

--- src/components/StationEtds.js
+++ src/components/StationEtds.js
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-export default function StationEtds({station}){
+import {fetchEtds} from '../lib/bartGateway';
+
+export default class StationEtds extends Component{
+  constructor(){
+    super();
+    this.state = {etds:false};
+  }
+
+  componentWillMount(){
+    fetchEtds(this.props.station)
+      .then( (etds)=> {
+        this.setState({ etds });
+      })
+      .catch( function(err){
+        console.error(err); // TODO: handle catch
+      });
+  }
+
+  render(){
+    return (
+      <section className="station-screen">
+        <Link to="/">
+          <h1 className="station-screen__title">
+            {this.props.station.name}
+          </h1>
+        </Link>
+        <div className="station-screen__body">
+          <Body etds={this.state.etds}/>
+        </div>
+      </section>
+    );
+  }
+}
+
+function Body({etds}){
+  if( !etds ){
+    return <Loading/>;
+  }
+
+  const items = etds.map( (etd,ix) => <Estimate key={ix} etd={etd} /> );
   return (
-    <section className="station-screen">
-      <Link to="/">
-        <h1 className="station-screen__title">
-          {station.name}
-        </h1>
-      </Link>
-    </section>
+    <ul className="etds-list">
+      {items}
+    </ul>
   );
 }
 
+function Loading(){
+  return (
+    <div className="loading__spinner"/>
+  );
+}
+
+function Estimate({etd}){
+  const label = `${etd.dest.name}: ${etd.minutes}`;
+  const className = `etds-list__etd -line-${etd.lineColor}`;
+  return (
+    <li className={className}>
+      {label}
+    </li>
+  );
+}
---
+++ src/lib/bartGateway.js
@@ -0,0 +1,70 @@
+const ETD_URL = "https://api.bart.gov/api/etd.aspx"
+const API_KEY="MW9S-E7SL-26DU-VV8V"
+
+import * as stationsRepo from './stationsRepo';
+
+export function fetchEtds(station){
+  // yes, there are probably 413 npm modules to do this for me.
+  const querySection = [
+    'cmd=etd',
+    `orig=${encodeURIComponent(station.abbr)}`,
+    `key=${API_KEY}`
+  ].join('&');
+
+  const fullUrl = `${ETD_URL}?${querySection}`;
+
+  return fetch(fullUrl)
+    .then(parseXml)
+    .then(parseEtds)
+    .then(sortEtds);
+}
+
+function parseXml(response){
+  return response.text().then(function (str) {
+    return (new window.DOMParser()).parseFromString(str, "text/xml");
+  });
+}
+
+function parseEtds(doc){
+  return flatMapNodes(
+    doc.getElementsByTagName('etd'),
+    parseEtd
+  );
+}
+
+function sortEtds(etds){
+  return etds.sort( (a,b)=> a.minutes - b.minutes );
+}
+
+function parseEtd(etd){
+  const destAbbr = pluckTextFromNode(etd, 'abbreviation');
+  const destStation = stationsRepo.stationByAbbr(destAbbr);
+  return mapNodes(etd.getElementsByTagName('estimate'), function(estimates){
+    return {
+      dest: destStation,
+      minutes: parseMinutes(pluckTextFromNode(estimates,'minutes')),
+      lineColor: pluckTextFromNode(estimates,'color').toLowerCase(),
+      length: pluckTextFromNode(estimates,'length')
+    };
+  });
+}
+
+function mapNodes(nodes,fn){
+  return Array.from(nodes).map(fn);
+}
+
+function flatMapNodes(nodes,fn){
+  return Array.from(nodes).reduce(function(acc,val) {
+    return acc.concat(fn(val));
+  },[]);
+}
+
+function pluckTextFromNode(node, tagName){
+  return node.getElementsByTagName(tagName)[0].textContent;
+}
+  
+function parseMinutes(minutesString){
+  const mins = parseInt(minutesString,10);
+  return isNaN(mins) ? 0 : mins;
+}
+
