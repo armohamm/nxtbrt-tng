fad18954cf932812c14c0fc936cc5b22688c1161
Pete Hodgson <git@thepete.net>
2018-02-04 11:07:42 -0800

    better Raven setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import Raven from 'raven-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
 import './index.css';
 
-window.Raven.config('https://d43ffc4ffbe0437f8594b4816d570d7c@sentry.io/282773').install()
+Raven.config('https://d43ffc4ffbe0437f8594b4816d570d7c@sentry.io/282773').install();
 
 ReactDOM.render(
   <App/>,
