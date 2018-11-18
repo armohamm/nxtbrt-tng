1897019634a159d51389de3629ed320db1afe666
Pete Hodgson <git@thepete.net>
2018-11-17 22:07:49 -0800

    switch to non-legacy Sentry JS SDK

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import Raven from 'raven-js';
+import * as Sentry from '@sentry/browser';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
 import './index.css';
 
-Raven.config('https://d43ffc4ffbe0437f8594b4816d570d7c@sentry.io/282773').install();
+Sentry.init({ dsn: 'https://d43ffc4ffbe0437f8594b4816d570d7c@sentry.io/282773' });
 
 ReactDOM.render(
   <App/>,
